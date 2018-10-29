872d8f2b4e7c2e7e0dbc66ab3ad36668fedbf5b4
deartune <deartune@gmail.com>
2018-10-29 13:09:16 +0900

    1

--- app/src/main/java/com/jmk/edu/edittextandradiogroup/MainActivity.java
+++ app/src/main/java/com/jmk/edu/edittextandradiogroup/MainActivity.java
@@ -12,40 +12,35 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener,View.OnKeyListener {
-    EditText EditTextName,EditTextAge;
-    Button button1,button2,button3;
-    RadioGroup rg0, rg1;
-    TextView tv;
-    String gender, job, name, age;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditTextName = (EditText) findViewById(R.id.EditTextName);
-        EditTextName.setOnKeyListener(this);
-        EditTextAge = (EditText) findViewById(R.id.EditTextAge);
-        EditTextAge.setOnKeyListener(this);
-        button1 = (Button) findViewById(R.id.button1);
+
+        Button button1 = (Button) findViewById(R.id.button1);
         button1.setOnClickListener(this);
-        button2 = (Button) findViewById(R.id.button2);
+        Button button2 = (Button) findViewById(R.id.button2);
         button2.setOnClickListener(this);
-        button3 = (Button) findViewById(R.id.button3);
+        Button button3 = (Button) findViewById(R.id.button3);
         button3.setOnClickListener(this);
-        rg0 = (RadioGroup) findViewById(R.id.gender);
-        rg1 = (RadioGroup) findViewById(R.id.job);
-        tv = (TextView) findViewById(R.id.textView2);
 
     }
 
     @Override
     public void onClick(View v) {
+        TextView tv=(TextView)findViewById(R.id.textView2);
+        RadioGroup rg0 =(RadioGroup)findViewById(R.id.gender);
         int id = rg0.getCheckedRadioButtonId();
         RadioButton rb = (RadioButton) findViewById(id);
+        RadioGroup rg1 =(RadioGroup)findViewById(R.id.job);
         int id1 = rg1.getCheckedRadioButtonId();
         RadioButton rb1 = (RadioButton) findViewById(id1);
+        String name=((EditText)findViewById(R.id.EditTextName)).getText().toString();
+        String age=((EditText)findViewById(R.id.EditTextAge)).getText().toString();
 
         switch (v.getId()) {
             case R.id.button1 :
@@ -63,20 +58,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.button3 :
                 String text2 = "성별:"+ rb.getText().toString() + "직업:"+rb1.getText().toString();
                 text2 = "이름 :"+name +"나이 : "+age + text2;
-                tv.setText(text2); break ;}}
+                tv.setText(text2); break ;}}}
 
 
 
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                switch (v.getId()){
-                    case R.id.EditTextName:
-                        name=EditTextName.getText().toString();
-                        break;
 
-                    case R.id.EditTextAge:
-                        age=EditTextAge.getText().toString();
-                        break;
-                }
-                return false;
-            }
-        }
+
+
+
+
